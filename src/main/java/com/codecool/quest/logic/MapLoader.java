36fd804954759037a51a75e35ab2d2ba805575fb
Rajnai Máté <ramate.rajnai@gmail.com>
2020-01-15 12:44:42 +0100

    Remove comments

--- src/main/java/com/codecool/quest/logic/MapLoader.java
+++ src/main/java/com/codecool/quest/logic/MapLoader.java
@@ -17,19 +17,15 @@ public class MapLoader {
         int width = scanner.nextInt();
         int height = scanner.nextInt();
 
-        scanner.nextLine(); // empty line
-        // A map.txt alapján a memóriában létrehozzuk a map objektumot, minden egyes mezőre a txt-ben a memóriában eltárolt map objektumra hivatkozik egy-egy cell objektum
+        scanner.nextLine();
         GameMap map = new GameMap(width, height, CellType.EMPTY);
         for (int y = 0; y < height; y++) {
             String line = scanner.nextLine();
             for (int x = 0; x < width; x++) {
                 if (x < line.length()) {
-                    // A map.txt minden mezejében létrehozza a cell objektumot aminek Cell a típusa
                     Cell cell = map.getCell(x, y);
                     switch (line.charAt(x)) {
                         case ' ':
-                            // a cellára,  a hammer-re vagy a skeleton objektumra hivatkozva, akkor null-t kapunk vissza
-                            // TESZTELD
                             cell.setType(CellType.EMPTY);
                             break;
                         case '#':
