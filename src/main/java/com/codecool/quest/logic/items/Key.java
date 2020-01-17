d0f7fa22687f3c3c3f0b4791f35b9e356f6fa881
Rajnai Máté <ramate.rajnai@gmail.com>
2020-01-17 21:05:06 +0100

    Create addKey method

--- src/main/java/com/codecool/quest/logic/items/Key.java
+++ src/main/java/com/codecool/quest/logic/items/Key.java
@@ -2,10 +2,23 @@ package com.codecool.quest.logic.items;
 
 import com.codecool.quest.logic.Cell;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Key extends Item {
+
+    private static List<Key> keys = new ArrayList<>();
 
     public Key(Cell cell) {
         super(cell);
+    }
+
+    public static void addKey(Key key) {
+        keys.add(key);
+    }
+
+    public void openDoor(Key key) {
+
     }
 
     @Override
