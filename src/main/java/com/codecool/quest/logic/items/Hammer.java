bfbf2474bb48f52dc7ac081c96574c4ff9bedb47
Rajnai Máté <ramate.rajnai@gmail.com>
2020-01-17 21:03:56 +0100

    Add created coin object to List

--- src/main/java/com/codecool/quest/logic/items/Hammer.java
+++ src/main/java/com/codecool/quest/logic/items/Hammer.java
@@ -2,10 +2,19 @@ package com.codecool.quest.logic.items;
 
 import com.codecool.quest.logic.Cell;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hammer extends Item {
+
+    private static List<Hammer> hammers = new ArrayList<>();
 
     public Hammer(Cell cell) {
         super(cell);
+    }
+
+    public static void addHammer(Hammer hammer) {
+        hammers.add(hammer);
     }
 
     @Override
