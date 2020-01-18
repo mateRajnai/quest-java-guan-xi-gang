c3d643ef9a99fce48d449a290628f19106f569e0
botond <belgianmaniax@gmail.com>
2020-01-18 15:58:06 +0100

    Extract visual-related fields and methods to Visuals

--- src/main/java/com/codecool/quest/Main.java
+++ src/main/java/com/codecool/quest/Main.java
@@ -1,6 +1,5 @@
 package com.codecool.quest;
 
-import com.codecool.quest.logic.Cell;
 import com.codecool.quest.logic.CellType;
 import com.codecool.quest.logic.GameMap;
 import com.codecool.quest.logic.MapLoader;
@@ -12,17 +11,9 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.Optional;
@@ -33,17 +24,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Main extends Application {
     GameMap map = MapLoader.loadMap(1);
-    Canvas canvas = new Canvas(
-            map.getWidth() * Tiles.TILE_WIDTH,
-            map.getHeight() * Tiles.TILE_WIDTH);
-    GraphicsContext context = canvas.getGraphicsContext2D();
-    Label healthLabel = new Label();
-    Button pickUpButton = new Button("Pick up");
-    Visuals visuals = new Visuals();
-
-    ListView<String> inventory = new ListView<>();
-
-    Label characterNameLabel = new Label("hackerman");
+    Visuals visuals;
     ScheduledExecutorService botActuator = Executors.newSingleThreadScheduledExecutor();
 
     public static void main(String[] args) {
@@ -52,75 +33,36 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        GridPane ui = createUI();
-        BorderPane borderPane = createBorderPane(ui);
-        preparePickUpButton(borderPane);
-        Scene scene = createScene(borderPane);
-        prepareStage(primaryStage, scene);
-        refresh();
+        visuals = new Visuals(map);
+        Scene scene = visuals.getScene();
+        initPickUpButton();
+        scene.setOnKeyPressed(this::onKeyPressed);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Codecool quest");
+        primaryStage.setOnCloseRequest(windowEvent -> botActuator.shutdown());
+        visuals.refresh();
         primaryStage.show();
-        borderPane.requestFocus();
+        visuals.getLayout().requestFocus();
         setCharacterName();
         activateBots();
     }
 
-    private GridPane createUI() {
-        GridPane ui = new GridPane();
-
-        ColumnConstraints col1 = new ColumnConstraints(85);
-        ui.getColumnConstraints().add(col1);
-
-        ui.setPrefWidth(200);
-        ui.setPadding(new Insets(10));
-
-        ui.add(new Label("Epic name: "), 0, 0);
-        ui.add(characterNameLabel, 1, 0);
-        ui.add(new Label("Health: "), 0, 1);
-        ui.add(healthLabel, 1, 1);
-
-        ui.add(pickUpButton, 0, 2);
-        ui.add(inventory, 0, 3);
-        inventory.setPrefWidth(30);
-        inventory.setPrefHeight(70);
-
-        return ui;
-    }
-
-    private BorderPane createBorderPane(GridPane ui) {
-        BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(canvas);
-        borderPane.setRight(ui);
-        return borderPane;
-    }
-
-    private void preparePickUpButton(BorderPane borderPane) {
+    private void initPickUpButton() {
         ObservableList<String> items = FXCollections.observableArrayList();
-        pickUpButton.setOnAction(actionEvent -> {
+        visuals.getPickUpButton().setOnAction(actionEvent -> {
             addItemToInventory(map, "hammer", items);
             addItemToInventory(map, "key", items);
             addItemToInventory(map, "coin", items);
             pickUpCoins(items);
-            borderPane.requestFocus();
+            visuals.getLayout().requestFocus();
         });
-    }
-
-    private Scene createScene(BorderPane borderPane) {
-        Scene scene = new Scene(borderPane);
-        scene.setOnKeyPressed(this::onKeyPressed);
-        return scene;
-    }
-
-    private void prepareStage(Stage primaryStage, Scene scene) {
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Codecool Quest");
-        primaryStage.setOnCloseRequest(windowEvent -> botActuator.shutdown());
     }
 
     private void addItemToInventory(GameMap map, String itemToBeAdd, ObservableList<String> items) {
         try {
             if (map.getPlayer().getCell().getItem().pickUpItem(map, itemToBeAdd)) {
                 items.add(itemToBeAdd);
-                inventory.setItems(items);
+                visuals.getInventory().setItems(items);
             }
         } catch (NullPointerException ignored) {
         }
@@ -129,7 +71,7 @@ public class Main extends Application {
     private void pickUpCoins(ObservableList<String> items) {
         if (map.getPlayer().getCell().getTileName().equals("coins")) {
             items.add("coins");
-            inventory.setItems(items);
+            visuals.getInventory().setItems(items);
             map.getPlayer().getCell().setType(CellType.FLOOR);
         }
     }
@@ -137,7 +79,7 @@ public class Main extends Application {
     private void setCharacterName() {
         TextInputDialog dialog = visuals.getCharacterNameDialog();
         Optional<String> result = dialog.showAndWait();
-        result.ifPresent(name -> this.characterNameLabel.setText(name));
+        result.ifPresent(name -> visuals.setCharacterName(name));
     }
 
     private void activateBots() {
@@ -150,7 +92,7 @@ public class Main extends Application {
         Bat.getBats().forEach(Bat::move);
         Duck.getDucks().forEach(Duck::move);
         Golem.getGolems().forEach(Golem::attackIfPlayerNextToIt);
-        refresh();
+        visuals.refresh();
     }
 
     private void onKeyPressed(KeyEvent keyEvent) {
@@ -169,26 +111,8 @@ public class Main extends Application {
                 map.getPlayer().move(1, 0);
                 break;
         }
-        refresh();
+        visuals.refresh();
         checkEndGame();
-    }
-
-    private void refresh() {
-        context.setFill(Color.BLACK);
-        context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        for (int x = 0; x < map.getWidth(); x++) {
-            for (int y = 0; y < map.getHeight(); y++) {
-                Cell cell = map.getCell(x, y);
-                if (cell.getActor() != null) {
-                    Tiles.drawTile(context, cell.getActor(), x, y);
-                } else if (cell.getItem() != null) {
-                    Tiles.drawTile(context, cell.getItem(), x, y);
-                } else {
-                    Tiles.drawTile(context, cell, x, y);
-                }
-            }
-        }
-        healthLabel.setText("" + map.getPlayer().getHealth());
     }
 
     public void checkEndGame() {
@@ -202,7 +126,8 @@ public class Main extends Application {
                 map = MapLoader.loadMap(1);
             }
             botActuator = Executors.newSingleThreadScheduledExecutor();
-            refresh();
+            visuals = new Visuals(map);
+            visuals.refresh();
             activateBots();
         }
     }
--- src/main/java/com/codecool/quest/Visuals.java
+++ src/main/java/com/codecool/quest/Visuals.java
@@ -1,17 +1,73 @@
 package com.codecool.quest;
 
+import com.codecool.quest.logic.Cell;
+import com.codecool.quest.logic.GameMap;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
 public class Visuals {
-    private TextInputDialog characterNameDialog;
-    private Alert gameWonAlert;
 
-    public Visuals() {
-        setCharacterNameDialog();
-        setGameWonAlert();
+    private static Canvas canvas;
+    private static GraphicsContext context;
+    private static GridPane ui = new GridPane();
+    private static BorderPane layout = new BorderPane();
+    private static Scene scene;
+
+    private static Label healthLabel = new Label("Health: ");
+    private static Label healthPoints = new Label();
+    private static Label characterNameLabel = new Label("Epic name: ");
+    private static Label characterName = new Label("hackerman");
+    private static Button pickUpButton = new Button("Pick up");
+    private static ListView<String> inventory = new ListView<>();
+
+    private static TextInputDialog characterNameDialog;
+    private static Alert gameWonAlert;
+
+    private GameMap map;
+
+    static {
+        int canvasWidth = 800;
+        int canvasHeight = 640;
+        canvas = new Canvas(canvasWidth, canvasHeight);
+        context = canvas.getGraphicsContext2D();
+        initUI();
+        layout.setCenter(canvas);
+        layout.setRight(ui);
+        scene = new Scene(layout);
+        initCharacterNameDialog();
+        initGameWonAlert();
     }
 
-    private void setCharacterNameDialog() {
+    public Visuals(GameMap map) {
+        this.map = map;
+    }
+
+    private static void initUI() {
+        ColumnConstraints col1 = new ColumnConstraints(85);
+        ui.getColumnConstraints().add(col1);
+
+        ui.setPrefWidth(200);
+        ui.setPadding(new Insets(10));
+
+        ui.add(characterNameLabel, 0, 1);
+        ui.add(characterName, 1, 1);
+        ui.add(healthLabel, 0, 0);
+        ui.add(healthPoints, 1, 0);
+
+        ui.add(pickUpButton, 0, 2);
+        ui.add(inventory, 0, 3);
+        inventory.setPrefWidth(30);
+        inventory.setPrefHeight(70);
+    }
+
+    private static void initCharacterNameDialog() {
         characterNameDialog = new TextInputDialog("hackerman");
         characterNameDialog.setTitle("Character setup");
         characterNameDialog.setHeaderText("Choose an epic name for your character!");
@@ -28,7 +84,7 @@ public class Visuals {
         });
     }
 
-    private void setGameWonAlert() {
+    private static void initGameWonAlert() {
         gameWonAlert = new Alert(
                 Alert.AlertType.INFORMATION,
                 "You have reached the stairs of wisdom!",
@@ -39,11 +95,49 @@ public class Visuals {
         gameWonAlert.setTitle("Epic victory message");
     }
 
+    public Button getPickUpButton() {
+        return pickUpButton;
+    }
+
+    public BorderPane getLayout() {
+        return layout;
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public ListView<String> getInventory() {
+        return inventory;
+    }
+
+    public void setCharacterName(String name) {
+        characterName.setText(name);
+    }
+
     public TextInputDialog getCharacterNameDialog() {
         return characterNameDialog;
     }
 
     public Alert getGameWonAlert() {
         return gameWonAlert;
+    }
+
+    public void refresh() {
+        context.setFill(Color.BLACK);
+        context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        for (int x = 0; x < map.getWidth(); x++) {
+            for (int y = 0; y < map.getHeight(); y++) {
+                Cell cell = map.getCell(x, y);
+                if (cell.getActor() != null) {
+                    Tiles.drawTile(context, cell.getActor(), x, y);
+                } else if (cell.getItem() != null) {
+                    Tiles.drawTile(context, cell.getItem(), x, y);
+                } else {
+                    Tiles.drawTile(context, cell, x, y);
+                }
+            }
+        }
+        healthPoints.setText(Integer.toString(map.getPlayer().getHealth()));
     }
 }
