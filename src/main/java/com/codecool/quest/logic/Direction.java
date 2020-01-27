2df47c6e173242a7b3eb848735ab9c4b5b6ea216
botond <belgianmaniax@gmail.com>
2020-01-27 14:17:27 +0100

    Fix direction vectors

--- src/main/java/com/codecool/quest/logic/Direction.java
+++ src/main/java/com/codecool/quest/logic/Direction.java
@@ -1,8 +1,8 @@
 package com.codecool.quest.logic;
 
 public enum Direction {
-    UP(0, 1), DOWN(0, -1), RIGHT(1, 0), LEFT(0, 1),
-    RIGHT_UP(1, 1), RIGHT_DOWN(1, -1), LEFT_UP(-1, 1), LEFT_DOWN(-1, -1);
+    UP(0, -1), DOWN(0, 1), RIGHT(1, 0), LEFT(0, 1),
+    RIGHT_UP(1, -1), RIGHT_DOWN(1, 1), LEFT_UP(-1, -1), LEFT_DOWN(-1, 1);
 
     private final int dx;
     private final int dy;
