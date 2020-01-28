9874a51a89ba6fcab31bca2ca239ba36c36ba31e
botond <belgianmaniax@gmail.com>
2020-01-28 14:44:11 +0100

    Edit VisualFrameWork

--- src/main/java/com/codecool/quest/VisualFrameWork.java
+++ src/main/java/com/codecool/quest/VisualFrameWork.java
@@ -10,6 +10,8 @@ import javafx.scene.paint.Color;
 public class VisualFrameWork {
 
     private Canvas canvas;
+    private final int CANVAS_WIDTH = 800;
+    private final int CANVAS_HEIGHT = 640;
     private GraphicsContext context;
     private BorderPane layout;
 
@@ -17,9 +19,7 @@ public class VisualFrameWork {
     private Scene scene;
 
     public VisualFrameWork(UI ui) {
-        int canvasWidth = 800;
-        int canvasHeight = 640;
-        canvas = new Canvas(canvasWidth, canvasHeight);
+        canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
         context = canvas.getGraphicsContext2D();
         layout = new BorderPane();
         layout.setCenter(canvas);
@@ -42,9 +42,9 @@ public class VisualFrameWork {
         for (int x = 0; x < ui.getMap().getWidth(); x++) {
             for (int y = 0; y < ui.getMap().getHeight(); y++) {
                 Cell cell = ui.getMap().getCell(x, y);
-                if (cell.getActor() != null) {
+                if (cell.hasActor()) {
                     Tiles.drawTile(context, cell.getActor(), x, y);
-                } else if (cell.getItem() != null) {
+                } else if (cell.hasItem()) {
                     Tiles.drawTile(context, cell.getItem(), x, y);
                 } else {
                     Tiles.drawTile(context, cell, x, y);
