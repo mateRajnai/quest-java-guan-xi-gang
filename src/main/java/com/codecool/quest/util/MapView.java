f1681a397d171252b1140fe36c4e7027524d1394
botond <belgianmaniax@gmail.com>
2020-01-28 21:19:24 +0100

    Implement horizontal adjustment

--- src/main/java/com/codecool/quest/util/MapView.java
+++ src/main/java/com/codecool/quest/util/MapView.java
@@ -6,6 +6,8 @@ import com.codecool.quest.logic.actors.Player;
 public class MapView {
     private final static int CANVAS_WIDTH = 25;
     private final static int CANVAS_HEIGHT = 20;
+    private final static int WIDTH_BREAKPOINT = (int) (CANVAS_WIDTH * 0.75);
+    private final static int DELTA_WIDTH = CANVAS_WIDTH / 2;
     private final static int HEIGHT_BREAKPOINT = (int) (CANVAS_HEIGHT * 0.75);
     private final static int DELTA_HEIGHT = CANVAS_HEIGHT / 2;
 
@@ -20,6 +22,16 @@ public class MapView {
         this.player = map.getPlayer();
     }
 
+    private int getRelativeX() {
+        int x = player.getX();
+        if (x < WIDTH_BREAKPOINT)
+            return x;
+        else if (x < mapWidth - WIDTH_BREAKPOINT)
+            return ((x - WIDTH_BREAKPOINT) % DELTA_WIDTH) + (CANVAS_WIDTH - WIDTH_BREAKPOINT);
+        else
+            return CANVAS_WIDTH - (mapWidth - x);
+    }
+
     private int getRelativeY() {
         int y = player.getY();
         if (y < HEIGHT_BREAKPOINT)
@@ -28,6 +40,10 @@ public class MapView {
             return ((y - HEIGHT_BREAKPOINT) % DELTA_HEIGHT) + (CANVAS_HEIGHT - HEIGHT_BREAKPOINT);
         else
             return CANVAS_HEIGHT - (mapHeight - y);
+    }
+
+    public int minX() {
+        return player.getX() - getRelativeX();
     }
 
     public int minY() {
