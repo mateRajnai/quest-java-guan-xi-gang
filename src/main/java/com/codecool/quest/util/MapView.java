4658e6bd8ca09954cd5fe8f39d9837cf0060d66d
botond <belgianmaniax@gmail.com>
2020-01-28 22:01:01 +0100

    Reimplement screen adjustment

--- src/main/java/com/codecool/quest/util/MapView.java
+++ src/main/java/com/codecool/quest/util/MapView.java
@@ -6,47 +6,74 @@ import com.codecool.quest.logic.actors.Player;
 public class MapView {
     private final static int CANVAS_WIDTH = 25;
     private final static int CANVAS_HEIGHT = 20;
-    private final static int WIDTH_BREAKPOINT = (int) (CANVAS_WIDTH * 0.75);
-    private final static int DELTA_WIDTH = CANVAS_WIDTH / 2;
-    private final static int HEIGHT_BREAKPOINT = (int) (CANVAS_HEIGHT * 0.75);
-    private final static int DELTA_HEIGHT = CANVAS_HEIGHT / 2;
+    private final static int WIDTH_BREAKPOINT = (int) (CANVAS_WIDTH * 0.66);
+    private final static int HEIGHT_BREAKPOINT = (int) (CANVAS_HEIGHT * 0.66);
 
     private int mapWidth;
     private int mapHeight;
 
     private Player player;
 
+    private int minX;
+    private int minY;
+
     public MapView(GameMap map) {
         this.mapWidth = map.getWidth();
         this.mapHeight = map.getHeight();
         this.player = map.getPlayer();
+        this.minX = initialMinX();
+        this.minY = initialMinY();
     }
 
-    private int getRelativeX() {
+    private int initialMinX() {
         int x = player.getX();
-        if (x < WIDTH_BREAKPOINT)
-            return x;
-        else if (x < mapWidth - WIDTH_BREAKPOINT)
-            return ((x - WIDTH_BREAKPOINT) % DELTA_WIDTH) + (CANVAS_WIDTH - WIDTH_BREAKPOINT);
+        if (x < CANVAS_WIDTH / 2)
+            return 0;
+        else if (x < mapWidth - CANVAS_WIDTH / 2)
+            return x - CANVAS_WIDTH / 2;
         else
-            return CANVAS_WIDTH - (mapWidth - x);
+            return mapWidth - CANVAS_WIDTH;
     }
 
-    private int getRelativeY() {
+    private int initialMinY() {
         int y = player.getY();
-        if (y < HEIGHT_BREAKPOINT)
-            return y;
-        else if (y < mapHeight - HEIGHT_BREAKPOINT)
-            return ((y - HEIGHT_BREAKPOINT) % DELTA_HEIGHT) + (CANVAS_HEIGHT - HEIGHT_BREAKPOINT);
+        if (y < CANVAS_HEIGHT / 2)
+            return 0;
+        else if (y < mapHeight - CANVAS_HEIGHT / 2)
+            return y - CANVAS_HEIGHT / 2;
         else
-            return CANVAS_HEIGHT - (mapHeight - y);
+            return mapHeight - CANVAS_HEIGHT;
     }
 
     public int minX() {
-        return player.getX() - getRelativeX();
+        int x = player.getX();
+        if (minX == 0 && x < WIDTH_BREAKPOINT)
+            return minX;
+
+        int relativeX = x - this.minX;
+        if (relativeX < CANVAS_WIDTH - WIDTH_BREAKPOINT)
+            return --minX;
+        else if (relativeX < WIDTH_BREAKPOINT)
+            return minX;
+        else if (x < mapWidth - (CANVAS_WIDTH - WIDTH_BREAKPOINT))
+            return ++minX;
+        else
+            return minX;
     }
 
     public int minY() {
-        return player.getY() - getRelativeY();
+        int y = player.getY();
+        if (minY == 0 && y < HEIGHT_BREAKPOINT)
+            return minY;
+
+        int relativeY = y - this.minY;
+        if (relativeY < CANVAS_HEIGHT - HEIGHT_BREAKPOINT)
+            return --minY;
+        else if (relativeY < HEIGHT_BREAKPOINT)
+            return minY;
+        else if (y < mapHeight - (CANVAS_HEIGHT - HEIGHT_BREAKPOINT))
+            return ++minY;
+        else
+            return minY;
     }
 }
