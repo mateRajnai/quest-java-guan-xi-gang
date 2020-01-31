cef7552fb7f93e8e98d0cbc9c196914efdab6274
botond <belgianmaniax@gmail.com>
2020-01-31 13:24:59 +0100

    Add field isObstacle to CellType

--- src/main/java/com/codecool/quest/logic/CellType.java
+++ src/main/java/com/codecool/quest/logic/CellType.java
@@ -1,17 +1,23 @@
 package com.codecool.quest.logic;
 
 public enum CellType {
-    EMPTY("empty"),
-    FLOOR("floor"),
-    WALL("wall");
+    EMPTY("empty", true),
+    FLOOR("floor", false),
+    WALL("wall", true);
 
     private final String tileName;
+    private final boolean isObstacle;
 
-    CellType(String tileName) {
+    CellType(String tileName, boolean isObstacle) {
         this.tileName = tileName;
+        this.isObstacle = isObstacle;
     }
 
     public String getTileName() {
         return tileName;
+    }
+
+    public boolean isObstacle() {
+        return isObstacle;
     }
 }
