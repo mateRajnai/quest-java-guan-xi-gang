238a568ff199f48c0485f81690d646869ec6bc03
botond <belgianmaniax@gmail.com>
2020-02-01 19:49:47 +0100

    Set Skeleton.INITIAL_HEALTH & Skeleton.INITIAL_ATTACK_DAMAGE

--- src/main/java/com/codecool/quest/logic/mapentities/actors/Skeleton.java
+++ src/main/java/com/codecool/quest/logic/mapentities/actors/Skeleton.java
@@ -6,10 +6,15 @@ import com.codecool.quest.logic.util.Direction;
 
 public class Skeleton extends IntelligentFoe implements Vulnerable {
 
+    private static final int INITIAL_HEALTH = 10;
+    private static final int INITIAL_ATTACK_DAMAGE = 2;
+
     private Direction direction = Direction.LEFT;
 
     public Skeleton(Cell cell) {
         super(cell);
+        this.setHealth(INITIAL_HEALTH);
+        this.setAttackDamage(INITIAL_ATTACK_DAMAGE);
     }
 
     @Override
