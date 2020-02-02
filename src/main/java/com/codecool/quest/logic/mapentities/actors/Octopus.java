f975c3d046eede3364c56c5037168a6b8b179e53
botond <belgianmaniax@gmail.com>
2020-02-02 21:31:54 +0100

    Calculate Octopus.surroundings

--- src/main/java/com/codecool/quest/logic/mapentities/actors/Octopus.java
+++ src/main/java/com/codecool/quest/logic/mapentities/actors/Octopus.java
@@ -2,17 +2,24 @@ package com.codecool.quest.logic.mapentities.actors;
 
 import com.codecool.quest.logic.Cell;
 import com.codecool.quest.logic.mapentities.Vulnerable;
+import com.codecool.quest.logic.util.CellSurroundings;
 import com.codecool.quest.logic.util.Direction;
+
+import java.util.List;
 
 public class Octopus extends IntelligentFoe implements Vulnerable {
 
     private final static int INITIAL_HEALTH = 10;
     private final static int INITIAL_ATTACK_DAMAGE = 2;
+    private final static int DETECTION_BOX_SIZE = 3;
+
+    private List<Cell> surroundings;
 
     public Octopus(Cell cell) {
         super(cell);
         this.setHealth(INITIAL_HEALTH);
         this.setAttackDamage(INITIAL_ATTACK_DAMAGE);
+        surroundings = new CellSurroundings(this.cell, DETECTION_BOX_SIZE).getCells();
     }
 
     @Override
