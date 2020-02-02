9ccdda0a74c46750783e5665815b44c9a01d523a
botond <belgianmaniax@gmail.com>
2020-02-02 15:22:46 +0100

    Implement Cycle

---
+++ src/main/java/com/codecool/quest/logic/util/Cycle.java
@@ -0,0 +1,55 @@
+package com.codecool.quest.logic.util;
+
+public class Cycle<T> {
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        protected Node(T object) {
+            this.value = object;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        public T value() {
+            return value;
+        }
+
+        public Node<T> next() {
+            return next;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private Node<T> current;
+
+    public void add(T value) {
+        if (head == null) {
+            head = new Node<>(value);
+            tail = head;
+            head.setNext(tail);
+            tail.setNext(head);
+        }
+        else {
+            Node<T> current = head;
+            while (current != tail)
+                current = current.next();
+            Node<T> newNode = new Node<>(value);
+            tail.setNext(newNode);
+            newNode.setNext(head);
+        }
+    }
+
+    public T next() {
+        if (current == null)
+            current = head;
+        else
+            current = current.next();
+        return current.value();
+    }
+
+}
